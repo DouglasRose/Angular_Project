8cebd664b6c434104b48ff08b4777c6c40ebc212
Iryna Howarth <irynahowarth@gmail.com>
2016-03-11 10:07:14 +0000

    WIP: search from API is working/ pulling followers and repos - test not working

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var githubApp = angular.module('githubApp', ['ngRoute', 'githubControllers','githubServices']);
+var githubApp = angular.module('githubApp', ['ngRoute']);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,14 +1,48 @@
 'use strict';
 
-var githubControllers = angular.module('githubControllers',[]);
+githubApp.controller('ProfileListCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.query = 'iryna';
 
-githubControllers.controller('ProfileListCtrl',['$scope', 'Profile', function($scope, Profile) {
-  $scope.profiles = Profile.query();
+  $http(
+    {
+      method : "GET",
+      url : "https://api.github.com/search/users",
+      params: {
+        q: $scope.query,
+        order: 'acs',
+        client_id: '',
+        client_secret: ''
+      }
+    }
+  ).then(
+    function(results) {
+      $scope.profiles = results.data.items;
+      $scope.profiles_found = results.data.total_count;
+      console.log($scope.profiles);
+      $scope.profiles.forEach(function(obj, index){
+         $scope.searchUserDetails(obj, obj.login);
+      });
+
+    }
+  );
+
+  $scope.searchUserDetails = function(user,query){
+    $http(
+      {
+        method : "GET",
+        url : "https://api.github.com/users/"+query,
+        params: {
+          client_id: '',
+          client_secret: ''
+        }
+      }
+    ).then(
+      function(userDetails){
+        user.followers = userDetails.data.followers;
+        user.public_repos = userDetails.data.public_repos;
+      }
+    );
+  };
+
 }]);
 
-githubControllers.controller('ProfileDetailCtrl', ['$scope', 'Profile', function($scope, Profile) {
-  $scope.profile = Profile.get({login: login}, function(profile) {
-      $scope.followers = profile.followers;
-  });
-
-}]);
